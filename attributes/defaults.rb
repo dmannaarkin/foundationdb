7d8408f2506df3fbd03fdf2801154229974cab90
Kevin <kevin@wavefront.com>
2016-05-12 18:45:48 -0700

    Templatized foundationdb.conf.

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -1,50 +1,32 @@
 #
-# Copyright (c) 2014-2015, Kevin (penniesfromkevin)
+# Copyright (c) 2014-2016, Kevin (penniesfromkevin)
 #
 # Installations can be full or upgrade only (does not touch configurations)
 #default['foundationdb']['install_type'] = 'upgrade'
 default['foundationdb']['install_type'] = 'full'
 
 # Options
-default['foundationdb']['options'] = [
-  {
-    'name' => 'fdbmonitor',
-    'options' => {
-      'user' => 'foundationdb',
-      'group' => 'foundationdb'
-    }
-  },
-  {
-    'name' => 'general',
-    'options' => {
-      'restart_delay' => 60,
-      'cluster_file' => '/etc/foundationdb/fdb.cluster'
-    }
-  },
-  {
-    'name' => 'fdbserver',
-    'options' => {
-      'command' => '/usr/sbin/fdbserver',
-      'public_address' => 'auto:$ID',
-      'listen_address' => 'public',
-      'datadir' => '/mnt/fdb/$ID',
-      'logdir' => '/mnt/logs/fdb',
-      '# logsize' => '10MiB',
-      '# maxlogssize' => '100MiB',
-      '# machine_id' => '',
-      '# datacenter_id' => '',
-      '# class' => '',
-      '# memory' => '8GiB',
-      '# storage_memory' => '1GiB'
-    }
-  },
-  {
-    'name' => 'backup_agent',
-    'options' => {
-      'command' => '/usr/lib/foundationdb/backup_agent/backup_agent'
-    }
-  }
-]
+default['foundationdb']['fdbmonitor']['user'] = 'foundationdb'
+default['foundationdb']['fdbmonitor']['group'] = 'foundationdb'
+
+default['foundationdb']['general']['restart_delay'] = 10
+default['foundationdb']['general']['cluster_file'] = '/etc/foundationdb/fdb.cluster'
+
+default['foundationdb']['fdbserver']['command'] = '/usr/sbin/fdbserver'
+default['foundationdb']['fdbserver']['public_address'] = 'auto:$ID'
+default['foundationdb']['fdbserver']['listen_address'] = 'public'
+default['foundationdb']['fdbserver']['datadir'] = '/mnt/fdb/$ID'
+default['foundationdb']['fdbserver']['logdir'] = '/mnt/logs/fdb'
+default['foundationdb']['fdbserver']['logsize'] = nil  # '10MiB'
+default['foundationdb']['fdbserver']['maxlogssize'] = nil  # '100MiB'
+default['foundationdb']['fdbserver']['machine_id'] = nil
+default['foundationdb']['fdbserver']['datacenter_id'] = nil
+default['foundationdb']['fdbserver']['class'] = nil
+default['foundationdb']['fdbserver']['memory'] = nil  # '8GiB'
+default['foundationdb']['fdbserver']['storage_memory'] = nil  # '1GiB'
+
+default['foundationdb']['backup_agent']['command'] = '/usr/lib/foundationdb/backup_agent/backup_agent'
+default['foundationdb']['backup_agent']['logdir'] = '/mnt/logs/fdb'
 
 # ssd processes
 default['foundationdb']['processes'] = [
@@ -52,23 +34,23 @@ default['foundationdb']['processes'] = [
     'port' => 4500
   }
 ]
-
-# Optional datacenter ID and machine ID
-default['foundationdb']['datacenter_id'] = nil
-default['foundationdb']['machine_id'] = nil
-
-# These will change the default locations
-default['foundationdb']['data_dir'] = nil
-default['foundationdb']['log_dir'] = nil
+# Sample full process options:
+# {
+#   'id' => 'buffer',
+#   'port' => 4600,
+#   'file' => '/etc/foundationdb/memory.cluster',
+#   'class' => 'transaction',
+#   'memory' => 22,
+#   'storage_memory" => 13,
+#   'count' => 1
+# }
 
 # Explicit package URLs, if available; if these are provided, version and
 #   base_url are ignored.
 default['foundationdb']['clients_url'] = nil
 default['foundationdb']['server_url'] = nil
-
 # Manage the versions if explicit package URLs have not been provided.
-default['foundationdb']['version'] = '2.0.10'
-
+default['foundationdb']['version'] = '3.0.10'
 # Base URL for packages, if available; path up to, but not including, slash
 default['foundationdb']['base_url'] = nil
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Installs FoundationDB server.
 #
-# Copyright (c) 2014-2015, Kevin (penniesfromkevin)
+# Copyright (c) 2014-2016, Kevin (penniesfromkevin)
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -41,12 +41,7 @@ else
 end
 
 # See if FoundationDB has been previously installed (fdb.cluster exists)
-initial_file = '/etc/foundationdb/fdb.cluster'
-if File.exists?(initial_file)
-  initial_install = false
-else
-  initial_install = true
-end
+initial_install = File.exists?('/etc/foundationdb/fdb.cluster')
 
 dpkg_package 'foundationdb-server' do
   options '--force-confold'
@@ -54,148 +49,50 @@ dpkg_package 'foundationdb-server' do
   action [:install]
 end
 
-if node['foundationdb']['install_type'] == 'full'
-  foundationdb_conf = '/etc/foundationdb/foundationdb.conf'
-
-  if node['foundationdb']['data_dir']
-    Chef::Log.info 'Configuring FoundationDB data_dir'
-    directory node['foundationdb']['data_dir'] do
-      owner 'foundationdb'
-      group 'foundationdb'
-      mode 00777
-      recursive true
-      action :create
-    end
-
-    ruby_block 'Edit foundationdb.conf data_dir' do
-      block do
-        rc = Chef::Util::FileEdit.new(foundationdb_conf)
-        rc.search_file_replace_line("^datadir = /var/lib/foundationdb/data/.*", "datadir = #{node['foundationdb']['data_dir']}/$ID")
-        rc.write_file
-      end
-    end
+if node['foundationdb']['install_type'] == 'full' and not initial_install
+  template '/etc/foundationdb/foundationdb.conf' do
+    source 'foundationdb.conf.erb'
+    owner node['foundationdb']['fdbmonitor']['user']
+    group node['foundationdb']['fdbmonitor']['group']
+    mode 0644
   end
 
-  if node['foundationdb']['log_dir']
-    Chef::Log.info 'Configuring FoundationDB log_dir'
-    directory node['foundationdb']['log_dir'] do
-      owner 'foundationdb'
-      group 'foundationdb'
-      mode 00777
-      recursive true
-      action :create
-    end
-
-    ruby_block 'Edit foundationdb.conf log_dir' do
-      block do
-        rc = Chef::Util::FileEdit.new(foundationdb_conf)
-        rc.search_file_replace_line('^logdir = /var/log/foundationdb', "logdir = #{node['foundationdb']['log_dir']}")
-        rc.write_file
-      end
-    end
-  end
-
-  if node['foundationdb']['machine_id'] and node['foundationdb']['machine_id'] != ''
-    ruby_block 'Edit foundationdb.conf machine_id' do
-      block do
-        rc = Chef::Util::FileEdit.new(foundationdb_conf)
-        rc.search_file_replace_line('# machine_id =.*', "machine_id = #{node['foundationdb']['machine_id']}")
-        rc.write_file
-      end
-    end
-  end
-
-  if node['foundationdb']['datacenter_id'] and node['foundationdb']['datacenter_id'] != ''
-    ruby_block 'Edit foundationdb.conf datacenter_id' do
-      block do
-        rc = Chef::Util::FileEdit.new(foundationdb_conf)
-        rc.search_file_replace_line('# datacenter_id =.*', "datacenter_id = #{node['foundationdb']['datacenter_id']}")
-        rc.write_file
-      end
-    end
-  end
-
-  if node['foundationdb']['processes']
-    # Comment out the initial process; user may not want 4500
-    ruby_block 'Edit foundationdb.conf fdbserver.port' do
-      block do
-        rc = Chef::Util::FileEdit.new(foundationdb_conf)
-        rc.search_file_replace_line("^.fdbserver\.4500.", "##[fdbserver.4500]")
-        rc.write_file
-      end
-    end
-  end
-
-  # Create new processes
+  # Create new cluster files
   node['foundationdb']['processes'].each do |process|
     fdb_port = process['port']
-    if process.has_key?(:count)
-      fdb_count = process['count']
-    else
-      fdb_count = 1
-    end
-    process_options = ''
-    if process.has_key?(:id)
-      process_id = process['id']
-    else
-      process_id = 'local'
-    end
-    if process.has_key?(:string)
-      process_string = process['string']
-    else
-      process_string = 'fdbstring'
-    end
     if process.has_key?(:file)
-      # Processes with files are usually memory processes
       file process['file'] do
         content "#{process_id}:#{process_string}@#{node['ipaddress']}:#{fdb_port}"
-        owner 'foundationdb'
-        group 'foundationdb'
+        owner node['foundationdb']['fdbmonitor']['user']
+        group node['foundationdb']['fdbmonitor']['group']
         mode 00666
         action :create_if_missing
       end
-      process_options += "\ncluster_file = #{process['file']}"
-    elsif initial_install and not process.has_key?(:class)
-      # Processes without files are SSD processes
-      file initial_file do
-        content "#{process_id}:#{process_string}@#{node['ipaddress']}:#{fdb_port}"
-        owner 'foundationdb'
-        group 'foundationdb'
-        mode 00666
-        action :create
-      end
-      # Only do this once
-      initial_install = false
-    end
-    if process.has_key?(:memory)
-      process_options += "\nmemory = #{process['memory']}GiB"
-    end
-    if process.has_key?(:storage_memory)
-      process_options += "\nstorage_memory = #{process['storage_memory']}GiB"
-    end
-    if process.has_key?(:class)
-      process_options += "\nclass = #{process['class']}"
-    end
-    ruby_block "Edit foundationdb.conf for process settings #{fdb_port}" do
-      block do
-        (1..fdb_count).each do|i|
-          rc = Chef::Util::FileEdit.new(foundationdb_conf)
-          rc.insert_line_if_no_match("^.fdbserver\.#{fdb_port}.", "\n# Process port #{fdb_port}\n[fdbserver.#{fdb_port}]#{process_options}")
-          rc.write_file
-          fdb_port += 1
-        end
-      end
     end
   end
 
-  if node['foundationdb']['make_public']
-    Chef::Log.info 'Making FoundationDB node public'
-    execute 'make public' do
-      command "/usr/lib/foundationdb/make_public.py #{node['foundationdb']['make_public_options']}"
-      action :run
-      # temporary until idempotency is implemented
-      ignore_failure true
+  # Create log directories
+  [
+    node['foundationdb']['fdbserver']['datadir'],
+    node['foundationdb']['fdbserver']['logdir'],
+    node['foundationdb']['backup_agent']['logdir']
+  ].each do |directory_location|
+    directory directory_location do
+      only_if { directory_location }
+      owner node['foundationdb']['fdbmonitor']['user']
+      group node['foundationdb']['fdbmonitor']['group']
+      mode 00777
+      recursive true
+      action :create
     end
+  end
+
+  execute 'make public' do
+    only_if { node['foundationdb']['make_public'] }
+    command "/usr/lib/foundationdb/make_public.py #{node['foundationdb']['make_public_options']}"
+    action :run
+    # temporary until idempotency is implemented
+    ignore_failure true
   end
 end
 
